77216847e586d741c4bfd5d49d2c4220b511c2fc
zgsun <zgsun@haitaiinc.com>
2023-05-30 22:55:56 +0800

    构建页面1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+      <UseHeader></UseHeader>
+      <router-view></router-view>
+      <UseFooter v-show="$route.meta.show"></UseFooter>
   </div>
 </template>
 
 <script>
-
+import UseHeader from "@/components/Header/UseHeader.vue";
+import UseFooter from "@/components/Footer/UseFooter.vue";
 export default {
   name: 'App',
   components: {
+      UseHeader,
+      UseFooter
   }
 }
 </script>
